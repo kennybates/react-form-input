751bbc41c4297793cf4af4bbff7e90be3b5a06e4
Kenny Bates <kennyleebates@outlook.com>
2021-10-14 20:42:09 -0400

    Update App.js
    Create handleSubmit and inline functions to target value of name and email.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
 
 
 function App() {
+
+  // state variables
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && email){
+      const person = {firstName, email};
+      console.log(person);
+    }
+  };
+
+  
+
   return (
     <>
     <article>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <div className='form-control'>
           <label htmlFor='firstName'> Name : </label>
           <input 
             type='text' 
             id='text'
             name='firstName'
+            value={firstName}
+            onChange={(e)=> setFirstName(e.target.value)}
             />
         </div>
+
+        <div className='form-control'>
+          <label htmlFor='email'> email : </label>
+          <input 
+            type='text' 
+            id='text'
+            name='email'
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
+            />
+        </div>
+        <button type='submit' className='btn'> 
+          Submit
+        </button>
       </form>
     </article>
     </>
