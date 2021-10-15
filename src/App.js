86f4a3e47a1c99bd76375eeaeb6f28c62ae4a6aa
Kenny Bates <kennyleebates@outlook.com>
2021-10-14 20:55:25 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 
 function App() {
@@ -8,12 +8,20 @@ function App() {
   // state variables
   const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (firstName && email){
-      const person = {firstName, email};
+      const person = { id: new Date().getTime().toString(), firstName, email };
       console.log(person);
+      setPeople((people) => {
+        return [...people, person];
+      });
+      setFirstName('');
+      setEmail('');
+    } else {
+      console.log('empty value');
     }
   };
 
@@ -27,7 +35,7 @@ function App() {
           <label htmlFor='firstName'> Name : </label>
           <input 
             type='text' 
-            id='text'
+            id='firstName'
             name='firstName'
             value={firstName}
             onChange={(e)=> setFirstName(e.target.value)}
@@ -38,16 +46,26 @@ function App() {
           <label htmlFor='email'> email : </label>
           <input 
             type='text' 
-            id='text'
+            id='email'
             name='email'
             value={email}
             onChange={(e)=> setEmail(e.target.value)}
             />
         </div>
-        <button type='submit' className='btn'> 
+        <button type='submit'> 
           Submit
         </button>
       </form>
+      {
+        //Map people array to create list of people onSubmit
+        people.map((person, index) => {
+          const {id, firstName, email} = person; //destructor object
+          return <div className='item' key={id}>
+            <h4>{firstName}</h4>
+            <p>{email}</p>
+          </div>
+        })
+      }
     </article>
     </>
   );
